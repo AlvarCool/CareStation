630319c8a27c4eea20ac965c8852d62c71c0c35a
AlvarCool <76956101+AlvarCool@users.noreply.github.com>
2022-03-28 07:11:59 -0600

    Update ABC.php

--- pages/ABC.php
+++ pages/ABC.php
@@ -1,14 +1,8 @@
 <?php
-    $nombre = $_POST("nombre");
-    if($nombre != ""){
     include("alta-nodo.php")
     ?>
     <script>
         alert("SE REGISTRO EL NUEVO NODO")
     </script>
-    ?>
     <?php
-    }else{
-        alert("Favor de ingresar un nombre")
-    }
 ?>
