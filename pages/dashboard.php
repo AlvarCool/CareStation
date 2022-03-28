ff3d2f58c602560ab057d3ceeb8fcf08f237531d
AlvarCool <76956101+AlvarCool@users.noreply.github.com>
2022-03-28 00:09:09 -0600

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -110,7 +110,7 @@
                 </div>
                 <div class="col-4 text-end">
                   <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
-                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
+                    <img src="../assets/img/icons/ico_temp.png" class="navbar-brand-img h-100">
                   </div>
                 </div>
               </div>
@@ -129,7 +129,7 @@
                 </div>
                 <div class="col-4 text-end">
                   <div class="icon icon-shape bg-gradient-danger shadow-danger text-center rounded-circle">
-                    <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
+                    <img src="../assets/img/icons/ico_hum.png" class="navbar-brand-img h-100">
                   </div>
                 </div>
               </div>
@@ -150,7 +150,7 @@
                 </div>
                 <div class="col-4 text-end">
                   <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
-                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
+                    <img src="../assets/img/icons/ico_humsu.png" class="navbar-brand-img h-100">
                   </div>
                 </div>
               </div>
@@ -169,7 +169,7 @@
                 </div>
                 <div class="col-4 text-end">
                   <div class="icon icon-shape bg-gradient-warning shadow-warning text-center rounded-circle">
-                    <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
+                    <img src="../assets/img/icons/ico_uv.png" class="navbar-brand-img h-100">
                   </div>
                 </div>
               </div>
