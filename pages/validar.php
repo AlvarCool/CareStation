9f0a49de551e0347f5f9602b9e7240350e7116fb
AlvarCool <76956101+AlvarCool@users.noreply.github.com>
2022-03-28 00:31:40 -0600

    Update validar.php

--- pages/validar.php
+++ pages/validar.php
@@ -1,3 +1,14 @@
 <?php
-        header("location:dashboard.php"); 
+    $usuario = $_POST['usuario'];
+    $contraseña = $_POST['contraseña'];
+if($usuario == "admin" && $contraseña == "123"){
+    header("location:dashboard.php"); 
+}else{
+        include("index.php")
+        ?>
+        <script>
+                alert("Usuario y/o contraseña incorrecta");
+        </script>
+        <?php
+}
 ?>
